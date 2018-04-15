33edb0c9094ee74b6a33a9af3241ce56c85aab44
Anton Persson <pltxtra@vuknob.com>
2018-04-15 09:06:32 +0200

    Streamline base64.c and adapt it to our use-case.
    Signed-off-by: Anton Persson <pltxtra@vuknob.com>

--- src_jni/libsvg/base64.c
+++ src_jni/libsvg/base64.c
@@ -1,3 +1,24 @@
+/*
+
+  Changes:
+  Copyright © 2018 Anton Persson
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public License as
+   published by the Free Software Foundation; either version 2 of the
+   License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public
+   License along with this program; if not, write to the
+   Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+
+*/
 /* Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
@@ -14,21 +35,9 @@
  * limitations under the License.
  */
 
-/* base64 encoder/decoder. Originally part of main/util.c
- * but moved here so that support/ab and apr_sha1.c could
- * use it. This meant removing the apr_palloc()s and adding
- * ugly 'len' functions, which is quite a nasty cost.
- */
-
-#include "apr_base64.h"
-#if APR_CHARSET_EBCDIC
-#include "apr_xlate.h"
-#endif				/* APR_CHARSET_EBCDIC */
-
 /* aaaack but it's fast and const should make it shared text page. */
 static const unsigned char pr2six[256] =
 {
-#if !APR_CHARSET_EBCDIC
     /* ASCII table */
     64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
     64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
@@ -46,72 +55,14 @@ static const unsigned char pr2six[256] =
     64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
     64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
     64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64
-#else /*APR_CHARSET_EBCDIC*/
-    /* EBCDIC table */
-    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
-    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
-    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
-    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
-    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 62, 64,
-    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
-    64, 63, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
-    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
-    64, 26, 27, 28, 29, 30, 31, 32, 33, 34, 64, 64, 64, 64, 64, 64,
-    64, 35, 36, 37, 38, 39, 40, 41, 42, 43, 64, 64, 64, 64, 64, 64,
-    64, 64, 44, 45, 46, 47, 48, 49, 50, 51, 64, 64, 64, 64, 64, 64,
-    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
-    64,  0,  1,  2,  3,  4,  5,  6,  7,  8, 64, 64, 64, 64, 64, 64,
-    64,  9, 10, 11, 12, 13, 14, 15, 16, 17, 64, 64, 64, 64, 64, 64,
-    64, 64, 18, 19, 20, 21, 22, 23, 24, 25, 64, 64, 64, 64, 64, 64,
-    52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 64, 64, 64, 64, 64, 64
-#endif /*APR_CHARSET_EBCDIC*/
 };
 
-#if APR_CHARSET_EBCDIC
-static apr_xlate_t *xlate_to_ebcdic;
-static unsigned char os_toascii[256];
 
-APR_DECLARE(apr_status_t) apr_base64init_ebcdic(apr_xlate_t *to_ascii,
-                                             apr_xlate_t *to_ebcdic)
-{
-    int i;
-    apr_size_t inbytes_left, outbytes_left;
-    apr_status_t rv;
-    int onoff;
-
-    /* Only single-byte conversion is supported.
-     */
-    rv = apr_xlate_sb_get(to_ascii, &onoff);
-    if (rv) {
-        return rv;
-    }
-    if (!onoff) { /* If conversion is not single-byte-only */
-        return APR_EINVAL;
-    }
-    rv = apr_xlate_sb_get(to_ebcdic, &onoff);
-    if (rv) {
-        return rv;
-    }
-    if (!onoff) { /* If conversion is not single-byte-only */
-        return APR_EINVAL;
-    }
-    xlate_to_ebcdic = to_ebcdic;
-    for (i = 0; i < sizeof(os_toascii); i++) {
-        os_toascii[i] = i;
-    }
-    inbytes_left = outbytes_left = sizeof(os_toascii);
-    apr_xlate_conv_buffer(to_ascii, os_toascii, &inbytes_left,
-                          os_toascii, &outbytes_left);
-
-    return APR_SUCCESS;
-}
-#endif /*APR_CHARSET_EBCDIC*/
-
-APR_DECLARE(int) apr_base64_decode_len(const char *bufcoded)
+int base64_decode_len(const char *bufcoded)
 {
     int nbytesdecoded;
     register const unsigned char *bufin;
-    register apr_size_t nprbytes;
+    register size_t nprbytes;
 
     bufin = (const unsigned char *) bufcoded;
     while (pr2six[*(bufin++)] <= 63);
@@ -122,34 +73,13 @@ APR_DECLARE(int) apr_base64_decode_len(const char *bufcoded)
     return nbytesdecoded + 1;
 }
 
-APR_DECLARE(int) apr_base64_decode(char *bufplain, const char *bufcoded)
-{
-#if APR_CHARSET_EBCDIC
-    apr_size_t inbytes_left, outbytes_left;
-#endif				/* APR_CHARSET_EBCDIC */
-    int len;
-
-    len = apr_base64_decode_binary((unsigned char *) bufplain, bufcoded);
-#if APR_CHARSET_EBCDIC
-    inbytes_left = outbytes_left = len;
-    apr_xlate_conv_buffer(xlate_to_ebcdic, bufplain, &inbytes_left,
-                          bufplain, &outbytes_left);
-#endif				/* APR_CHARSET_EBCDIC */
-    bufplain[len] = '\0';
-    return len;
-}
-
-/* This is the same as apr_base64_decode() except:
- * - no \0 is appended
- * - on EBCDIC machines, the conversion of the output to ebcdic is left out
- */
-APR_DECLARE(int) apr_base64_decode_binary(unsigned char *bufplain,
-				   const char *bufcoded)
+int base64_decode(unsigned char *bufplain,
+		  const char *bufcoded)
 {
     int nbytesdecoded;
     register const unsigned char *bufin;
     register unsigned char *bufout;
-    register apr_size_t nprbytes;
+    register size_t nprbytes;
 
     bufin = (const unsigned char *) bufcoded;
     while (pr2six[*(bufin++)] <= 63);
@@ -188,67 +118,16 @@ APR_DECLARE(int) apr_base64_decode_binary(unsigned char *bufplain,
     return nbytesdecoded;
 }
 
-APR_DECLARE(char *) apr_pbase64_decode(apr_pool_t *p, const char *bufcoded)
-{
-    char *decoded;
-    int l;
-
-    decoded = (char *) apr_palloc(p, 1 + apr_base64_decode_len(bufcoded));
-    l = apr_base64_decode(decoded, bufcoded);
-    decoded[l] = '\0'; /* make binary sequence into string */
-
-    return decoded;
-}
-
 static const char basis_64[] =
     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
-APR_DECLARE(int) apr_base64_encode_len(int len)
+int base64_encode_len(int len)
 {
     return ((len + 2) / 3 * 4) + 1;
 }
 
-APR_DECLARE(int) apr_base64_encode(char *encoded, const char *string, int len)
-{
-#if !APR_CHARSET_EBCDIC
-    return apr_base64_encode_binary(encoded, (const unsigned char *) string, len);
-#else /* APR_CHARSET_EBCDIC */
-    int i;
-    char *p;
-
-    p = encoded;
-    for (i = 0; i < len - 2; i += 3) {
-	*p++ = basis_64[(os_toascii[string[i]] >> 2) & 0x3F];
-	*p++ = basis_64[((os_toascii[string[i]] & 0x3) << 4) |
-	                ((int) (os_toascii[string[i + 1]] & 0xF0) >> 4)];
-	*p++ = basis_64[((os_toascii[string[i + 1]] & 0xF) << 2) |
-	                ((int) (os_toascii[string[i + 2]] & 0xC0) >> 6)];
-	*p++ = basis_64[os_toascii[string[i + 2]] & 0x3F];
-    }
-    if (i < len) {
-	*p++ = basis_64[(os_toascii[string[i]] >> 2) & 0x3F];
-	if (i == (len - 1)) {
-	    *p++ = basis_64[((os_toascii[string[i]] & 0x3) << 4)];
-	    *p++ = '=';
-	}
-	else {
-	    *p++ = basis_64[((os_toascii[string[i]] & 0x3) << 4) |
-	                    ((int) (os_toascii[string[i + 1]] & 0xF0) >> 4)];
-	    *p++ = basis_64[((os_toascii[string[i + 1]] & 0xF) << 2)];
-	}
-	*p++ = '=';
-    }
-
-    *p++ = '\0';
-    return p - encoded;
-#endif				/* APR_CHARSET_EBCDIC */
-}
-
-/* This is the same as apr_base64_encode() except on EBCDIC machines, where
- * the conversion of the input to ascii is left out.
- */
-APR_DECLARE(int) apr_base64_encode_binary(char *encoded,
-                                      const unsigned char *string, int len)
+int base64_encode(char *encoded,
+		  const unsigned char *string, int len)
 {
     int i;
     char *p;
@@ -278,16 +157,4 @@ APR_DECLARE(int) apr_base64_encode_binary(char *encoded,
 
     *p++ = '\0';
     return (int)(p - encoded);
-}
-
-APR_DECLARE(char *) apr_pbase64_encode(apr_pool_t *p, const char *string)
-{
-    char *encoded;
-    int l = strlen(string);
-
-    encoded = (char *) apr_palloc(p, apr_base64_encode_len(l));
-    l = apr_base64_encode(encoded, string, l);
-    encoded[l] = '\0'; /* make binary sequence into string */
-
-    return encoded;
 }
